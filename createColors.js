d1287949d030e62c6b42faf5feb6ffaff41db7bd
asingh <asingh@qualys.com>
2018-02-27 08:52:44 +0530

    updated path

--- createColors.js
+++ createColors.js
@@ -1,5 +1,5 @@
-import rgbGenerator from ".internal/rgbGenerator";
-import rgb2Hex from ".internal/rgb2Hex"
+import rgbGenerator from "./.internal/rgbGenerator";
+import rgb2Hex from "./.internal/rgb2Hex"
 const createColors = (count) =>{
     let colors =[]
  for (let i=0; i<count; i++) {
