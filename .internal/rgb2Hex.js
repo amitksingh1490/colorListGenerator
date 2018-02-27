5f23b92b5976bf8d7196a339ca1c1a5bf7612b1d
asingh <asingh@qualys.com>
2018-02-27 09:11:31 +0530

    Added # in the begining of the hex color codes

--- .internal/rgb2Hex.js
+++ .internal/rgb2Hex.js
@@ -1,4 +1,4 @@
-const rgbToHex = (rgb) {
+const rgbToHex = (rgb) => {
     var hex = Number(rgb).toString(16);
     if (hex.length < 2) {
         hex = "0" + hex;
@@ -6,16 +6,16 @@ const rgbToHex = (rgb) {
     return hex;
 };
 
-const fullColorHex = (rgb) {
+const fullColorHex = (rgb) => {
     var matchColors = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
-    var match = matchColors.exec(color);
+    var match = matchColors.exec(rgb);
     if (match !== null) {
         var red = rgbToHex(match[1]);
         var green = rgbToHex(match[2]);
         var blue = rgbToHex(match[3]);
     }
 
-    return red+green+blue;
+    return "#"+red+green+blue;
 };
 
 export default fullColorHex;
--- .internal/rgbGenerator.js
+++ .internal/rgbGenerator.js
@@ -1,8 +1,8 @@
-const rgbGenerator() {
+const rgbGenerator = ()=> {
     let rgb = [];
     for(var i = 0; i < 3; i++) {
         rgb.push(Math.floor(Math.random() * 255));
     }
-    return 'rgb('+ rgb.join(',') +')';
+    return 'rgb('+ rgb.join(', ') +')';
 }
 export default rgbGenerator;
